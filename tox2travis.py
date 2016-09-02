188ee1ee3f906398e5ab3260dff276201bbe3a77
Gael Pasgrimaud <gael@gawel.org>
2016-09-02 10:55:52 +0200

    improve tox config

---
+++ tox2travis.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+TRAVIS_CONF = '''
+language: python
+python: 3.5
+
+sudo: false
+
+install:
+  - pip install tox
+script:
+  - tox
+env:
+'''
+
+if __name__ == '__main__':
+    import subprocess
+    p = subprocess.check_output('tox -l', shell=True)
+    with open('.travis.yml', 'w') as fd:
+        fd.write(TRAVIS_CONF)
+        for env in p.split('\n'):
+            env = env.strip()
+            if env and env not in ('travis',):
+                fd.write('  - TOXENV={}\n'.format(env))
